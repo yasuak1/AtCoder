081915951c4069dd279c7592418e8e7624ae464c
yasuak1 <yasuakimizuno36@gmail.com>
2022-06-19 18:45:32 +0900

    Modify

--- ABC/B/256/B.py
+++ ABC/B/256/B.py
@@ -1,12 +1,16 @@
-n = int(input())
-a = list(map(int, input().split()))
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
 
-cnt = 0
-for i in range(n):
-    pos = 0
-    for j in range(i, n):
-        pos += a[j]
-        if pos >= 4:
-            cnt += 1
-            break
-print(cnt)
+    cnt = 0
+    for i in range(n):
+        pos = 0
+        for j in range(i, n):
+            pos += a[j]
+            if pos >= 4:
+                cnt += 1
+                break
+    print(cnt)
+
+if __name__ == '__main__':
+    main()
