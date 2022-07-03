933dd03ce4ba7ceb1d2a62b4e4bf5dda1bc74d1b
yasuak1 <yasuakimizuno36@gmail.com>
2022-07-03 13:24:30 +0900

    solve B/258

---
+++ ABC/B/258/B.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    vector<string> pvec(n);
+    while(n--) cin >> pvec.at(i);
+
+    vector<int> x = []
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+
+        }
+    }
+}
---
+++ ABC/B/258/B.rb
@@ -0,0 +1,26 @@
+direct = [-1, 0, 1].repeated_permutation(2).to_a
+direct.delete([0, 0])
+p direct
+
+n = gets.chomp.to_i
+array = Array.new
+n.times do
+    array << gets.chomp
+end
+
+max_val = -1
+for i in 0...n do
+    for j in 0...n do
+        direct.each do |vec|
+            s = [i, j].dup
+            str = ""
+            n.times do
+                i += vec[0]
+                j += vec[1]
+                str += array[i%n][j%n]
+            end
+            max_val = [max_val, str.to_i].max
+        end
+    end
+end
+p max_val
